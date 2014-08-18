918e5813ed15bda7833daff40b95c9d233878f2f
tenjj <yuptyy@163.com>
2014-08-18 23:59:04 +0800

    add
    add

---
+++ 3.py
@@ -0,0 +1,15 @@
+﻿##目录树
+import os
+def travelTree(currentPath, count):
+    if not os.path.exists(currentPath):
+        return
+    if os.path.isfile(currentPath):
+        fileName = os.path.basename(currentPath)
+        print '\t' * count + '├── ' + fileName
+    elif os.path.isdir(currentPath):
+        print '\t' * count + '├── ' + currentPath
+        pathList = os.listdir(currentPath)
+        for eachPath in pathList:
+            travelTree(currentPath+'/'+eachPath, count+1)
+
+travelTree('D:\php study\php code',1)
